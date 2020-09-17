39700e464b2a1f2c93c77dcaa542e503c66ad306
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-09-17 16:05:46 -0700

    added in the landing page for the site and added static images and names

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {name: "salmon creek", image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "grandit hill", image: "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "mountain goat's rest", image: "https://images.pexels.com/photos/2422265/pexels-photo-2422265.jpeg?auto=compress&cs=tinysrgb&h=350"}
+  ];
+
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
 app.listen(3000, process.env.IP, function(){
   console.log("server started");
 });
