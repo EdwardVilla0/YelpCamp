1b9a74e1b226f074c86d8f908704049d02dc3467
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-09-23 14:28:13 -0700

    restructuring files

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose");
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useUnifiedTopology: true, useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,13 +10,6 @@ app.set("view engine", "ejs");
 
 
 //schema set up
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create(
 //   {
