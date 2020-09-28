7f61fb3902c077db1dee1c0118a127b425958f28
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-09-28 16:19:47 -0700

    protected 3 routes from being edited without authorization

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ var commentRoutes         = require("./routes/comments"),
     campgroundRoutes      = require("./routes/campgrounds"),
     indexRoutes           = require("./routes/index");
 
-app.use(function(req, res, next){
-  res.locals.currentUser = req.user;
-  next();
-});
+
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
 app.use(bodyParser.urlencoded({extended: true}));
@@ -40,6 +37,11 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
 
 app.use(indexRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
