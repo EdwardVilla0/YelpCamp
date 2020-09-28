5faa779b76c94c6c29ae7242197ba0c11b569591
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-09-28 13:32:55 -0700

    created middlewear in order to add authorization to the edit page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -51,13 +51,9 @@ router.get("/:id", function(req, res){
 });
 
 //edit campground route
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
   Campground.findById(req.params.id, function(err, foundCampground){
-    if(err){
-      res.redirect("/campgrounds");
-    }else{
       res.render("campgrounds/edit", {campground: foundCampground});
-    }
   });
 });
 
@@ -90,6 +86,24 @@ function isLoggedIn(req, res, next){
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if(err){
+        res.redirect("back");
+      }else{
+        if(foundCampground.author.id.equals(req.user._id) ){
+          next();
+        }else{
+          res.redirect("back");
+        }
+      }
+    });
+  }else{
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
