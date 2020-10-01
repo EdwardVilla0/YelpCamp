853079577c5709c11e873899f000331a78dd584f
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2020-10-01 12:19:57 -0700

    fixing env.port

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ app.use(indexRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 app.use("/campgrounds",campgroundRoutes);
 
-app.listen(3000, process.env.IP, function(){
+// app.listen(3000, process.env.IP, function(){
+//   console.log("server started");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
   console.log("server started");
 });
